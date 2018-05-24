e0e88563d6ff7b2b84667f7cfe68a84765cb3fe3
MudOnTire <895157882@qq.com>
2018-05-24 16:31:48 +0800

    完成bag

--- public/js/bag.js
+++ public/js/bag.js
@@ -3,4 +3,14 @@ $(function () {
     $('.page-arrow').on('click touch', function () {
         $.fn.fullpage.moveSectionDown();
     });
+    $('#collegeFrame').on('load',function(){
+        this.height=this.contentWindow.document.body.scrollHeight;
+    });
+    $('.jump-link').on('click', function(){
+        $.fn.fullpage.moveTo(1);
+    });
 });
+ 
+function pageUp(){
+    $.fn.fullpage.moveSectionUp();    
+}
